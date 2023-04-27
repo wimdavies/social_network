29f9377d385dfd342c4636d8dc01473eda721271
wimdavies <william.johnston.davies@gmail.com>
2023-04-27 17:28:17 +0100

    post_repository tests encoded

--- spec/post_repository_spec.rb
+++ spec/post_repository_spec.rb
@@ -14,5 +14,87 @@ RSpec.describe PostRepository do
     reset_posts_table
   end
 
-  # (your tests will go here).
+  context '#all' do
+    xit 'returns all the posts' do
+      repo = PostRepository.new
+
+      posts = repo.all
+
+      expect(posts.length).to eq 3
+
+      expect(posts[0].id).to eq '1'
+      expect(posts[0].title).to eq 'Birthday'
+      expect(posts[0].contents).to eq '30 today!'
+      expect(posts[0].number_of_views).to eq '15'
+      expect(posts[0].user_account_id).to eq '1'
+
+      expect(posts[1].id).to eq '2'
+      expect(posts[1].title).to eq 'Funeral'
+      expect(posts[1].contents).to eq 'Dead today!'
+      expect(posts[1].number_of_views).to eq '30'
+      expect(posts[1].user_account_id).to eq '1'
+
+      expect(posts[2].id).to eq '3'
+      expect(posts[2].title).to eq 'Off to the shops'
+      expect(posts[2].contents).to eq 'Got some beans'
+      expect(posts[2].number_of_views).to eq '500'
+      expect(posts[2].user_account_id).to eq '2'
+    end
+  end
+
+  context '#find' do
+    xit 'returns the first post' do
+      repo = PostRepository.new
+
+      post = repo.find(1)
+
+      expect(post.id).to eq '1'
+      expect(post.title).to eq 'Birthday'
+      expect(post.contents).to eq '30 today!'
+      expect(post.number_of_views).to eq '15'
+      expect(post.user_account_id).to eq '1'
+    end
+  end
+
+  context '#create' do
+    xit 'creates a new post' do
+      repo = PostRepository.new
+
+      post = Post.new
+      post.title = 'test post'
+      post.contents = 'please ignore'
+      post.number_of_views = 1
+      post.user_account_id = 3
+
+      repo.create(post)
+
+      posts = repo.all
+
+      post = posts.last
+
+      expect(post.id).to eq '4'
+      expect(post.title).to eq 'test post'
+      expect(post.contents).to eq 'please ignore'
+      expect(post.number_of_views).to eq '1'
+      expect(post.user_account_id).to eq '3'
+    end
+  end
+
+  context '#delete' do
+    xit 'deletes the first post' do
+      repo = PostRepository.new
+
+      repo.delete(1) # => nil
+
+      posts = repo.all
+
+      post = posts.first
+
+      expect(post.id).to eq '2'
+      expect(post.title).to eq 'Funeral'
+      expect(post.contents).to eq 'Dead today!'
+      expect(post.number_of_views).to eq '30'
+      expect(post.user_account_id).to eq '1'
+    end
+  end
 end
